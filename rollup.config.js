bdf9f2a5d9e6c7a4807598feef39dc3b4411d28c
vorotamoroz <vrtmrz@gmail.com>
2025-05-27 12:15:27 +0100

    Fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 //@ts-check
-import typescript from "rollup-plugin-typescript2";
+import typescript from "@rollup/plugin-typescript";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import replace from "@rollup/plugin-replace";
@@ -7,6 +7,7 @@ import fs from "node:fs";
 import { globSync } from "glob";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import { platform } from "node:os";
 
 const pkg = JSON.parse(fs.readFileSync("./package.json") + "");
 const LIB_VERSION = pkg?.name + "-" + pkg?.version;
@@ -23,15 +24,23 @@ export default {
     platform: "browser",
     input: Object.fromEntries(
         globSync("src/**/*.ts")
-            .filter((file) => !file.endsWith(".test.ts") && !file.endsWith(".bench.ts") && !file.endsWith(".d.ts"))
+            .filter(
+                (file) =>
+                    !file.endsWith(".test.ts") &&
+                    !file.endsWith(".bench.ts") &&
+                    !file.endsWith(".d.ts")
+            )
             .map((file) => [
                 // This remove `src/` as well as the file extension from each
                 // file, so e.g. src/nested/foo.js becomes nested/foo
-                path.relative("src", file.slice(0, file.length - path.extname(file).length)),
+                path.relative(
+                    "src",
+                    file.slice(0, file.length - path.extname(file).length)
+                ),
                 // This expands the relative paths to absolute paths, so e.g.
                 // src/nested/foo becomes /project/src/nested/foo.js
                 fileURLToPath(new URL(file, import.meta.url)),
-            ]),
+            ])
     ),
     output: [
         {
@@ -52,14 +61,33 @@ export default {
     external: [/node_modules/],
     plugins: [
         typescript({
-            tsconfigOverride: {
-                platform: "browser",
-                compilerOptions: {
-                    declaration: true,
-                    sourceMap: true,
-                    outDir: "dist/types",
-                },
+            compilerOptions: {
+                declaration: true,
+                declarationDir: "./dist",
+                outDir: "./dist",
+                sourceMap: true,
             },
+            // noForceEmit: true,
+            // platform: "browser",
+            // compilerOptions:{
+            //     declaration: true,
+            //     sourceMap: true,
+            //     outDir: "dist/types",
+            //     // target: "es2020",
+            //     // module: "esnext",
+            //     // moduleResolution: "node",
+            //     // lib: ["es2020", "dom"],
+            //     // allowJs: true,
+            //     // checkJs: true,
+            // }
+            // tsconfigOverride: {
+            //     platform: "browser",
+            //     compilerOptions: {
+            //         // declaration: true,
+            //         sourceMap: true,
+            //         outDir: "dist/types",
+            //     },
+            // },
         }),
         nodeResolve({
             browser: true,
