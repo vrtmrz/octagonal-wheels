8e6efa091f5abb84de354ccb9d124546dea1e80b
vorotamoroz <vrtmrz@gmail.com>
2025-06-06 11:19:47 +0000

    Fix test of  `openssl` command. Update dependency and bump

--- serverTest/openSSLCompat.ts
+++ serverTest/openSSLCompat.ts
@@ -6,29 +6,15 @@ import process from "node:process";
 
 const opensslPath = process.env.OPENSSL_PATH || "openssl";
 console.log(`Using OpenSSL: ${opensslPath}`);
+await processCommand("version").then(() => {
+    console.log("**OpenSSL version command executed successfully.");
+}).catch((err) => {
+    console.error("**Error during OpenSSL version command execution:", err);
+    // process.exit(1);
+});
 
-// For Convenience, use readme.md
-const testFile = new Uint8Array(fs.readFileSync("../readme.md"));
 
-const passphrase = "testPassphrase";
-const iterations = 100_000;
-
-console.log("Starting CBC encryption and decryption...");
-const cbcEncrypted = await OpenSSLCompat.CBC.encryptCBC(testFile, passphrase, iterations);
-
-fs.writeFileSync("cbcEncrypted.bin", cbcEncrypted);
-
-console.log("CBC Encrypted file created: cbcEncrypted.bin");
-// Cleanup
-function ignoreFailure(func: () => void) {
-    try {
-        func();
-    } catch (e) {
-        // Ignore errors
-        // console.error("Error during cleanup:", e);
-    }
-}
-async function processCommand(command: string): Promise<void> {
+function processCommand(command: string): Promise<void> {
     return new Promise((resolve, reject) => {
         const child = spawn(opensslPath, [command], {
             stdio: "inherit",
@@ -43,7 +29,6 @@ async function processCommand(command: string): Promise<void> {
         child.on("exit", (code) => {
             if (code !== 0) {
                 console.error(`OpenSSL command failed with exit code ${code}: ${command}`);
-                console.error(`OpenSSL command failed with exit code ${code}`);
                 reject(new Error(`OpenSSL command failed with exit code ${code}`));
             } else {
                 console.log(`OpenSSL command executed successfully: ${command}`);
@@ -53,28 +38,50 @@ async function processCommand(command: string): Promise<void> {
     });
 }
 
-await processCommand("version").then(() => {
-    console.log("**OpenSSL version command executed successfully.");
-}).catch((err) => {
-    console.error("**Error during OpenSSL version command execution:", err);
-    // process.exit(1);
-});
-
-const command = `enc -d -aes-256-cbc -in cbcEncrypted.bin -out cbcDecrypted.md -k ${passphrase} -pbkdf2 -md sha256 -iter ${iterations}`;
-await processCommand(command).then(() => {
-    console.log("**OpenSSL CBC command executed successfully.");
-    const decryptedContent = fs.readFileSync("cbcDecrypted.md", "utf-8");
-    // console.log("Decrypted content:", decryptedContent);
-    if (decryptedContent === testFile.toString()) {
-        console.log("**Decryption successful, content matches original.");
-    } else {
-        console.error("**Decryption failed, content does not match original.");
+function ignoreFailure(func: () => void) {
+    try {
+        func();
+    } catch (e) {
+        // Ignore errors
+        // console.error("Error during cleanup:", e);
     }
-}).catch((err) => {
-    console.error("**Error during OpenSSL CBC command execution:", err);
-}).finally(() => {
-    console.log("**OpenSSL CBC command finished.");
-    ignoreFailure(() => fs.unlinkSync("cbcEncrypted.bin"));
-    ignoreFailure(() => fs.unlinkSync("cbcDecrypted.md"));
-});
+}
 
+async function checkCBC(filename: string, passphrase: string) {
+
+    const testFile = fs.readFileSync(filename);
+
+    // const passphrase = "testPassphrase";
+    const iterations = 100_000;
+
+    console.log("Starting CBC encryption and decryption...");
+    const cbcEncrypted = await OpenSSLCompat.CBC.encryptCBC(new Uint8Array(testFile), passphrase, iterations);
+
+    fs.writeFileSync("cbcEncrypted.bin", cbcEncrypted);
+
+    console.log("CBC Encrypted file created: cbcEncrypted.bin");
+
+
+    const command = `enc -d -aes-256-cbc -in cbcEncrypted.bin -out cbcDecrypted.md -k ${passphrase} -pbkdf2 -md sha256 -iter ${iterations}`;
+    await processCommand(command).then(() => {
+        console.log("**OpenSSL CBC command executed successfully.");
+        const decryptedContent = fs.readFileSync("cbcDecrypted.md", "utf-8");
+        // console.log("Decrypted content:", decryptedContent);
+        if (decryptedContent === testFile.toString()) {
+            console.log("**Decryption successful, content matches original.");
+        } else {
+            console.error("**Decryption failed, content does not match original.");
+        }
+    }).catch((err) => {
+        console.error("**Error during OpenSSL CBC command execution:", err);
+    }).finally(() => {
+        console.log("**OpenSSL CBC command finished.");
+        // Cleanup
+        ignoreFailure(() => fs.unlinkSync("cbcEncrypted.bin"));
+        ignoreFailure(() => fs.unlinkSync("cbcDecrypted.md"));
+    });
+
+};
+
+// For Convenience, use README.md
+await checkCBC("../README.md", "testPassphrase");
