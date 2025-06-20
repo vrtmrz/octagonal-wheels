8d16eaf12e6cd30c8c550f2339c59ef9c2cd517e
vorotamoroz <vrtmrz@gmail.com>
2025-06-20 12:16:04 +0100

    Fix exporting

--- globDist.mjs
+++ globDist.mjs
@@ -20,27 +20,45 @@ const map = files.flatMap((file) => {
     if (file == "index.js") {
         return [];
     }
+    if (file.endsWith("xxhash-wasm.js")) {
+        // Skip xxhash-wasm.js as it is not a module
+        return [];
+    }
     let exportPath = file.replace(/\.js$/, "");
+    let typePath = file.replace(/\.js$/, ".d.ts");
+    const writeTypePath = `./${distDirName}/${typePath}`;
+    const writeImportPath = `./${distDirName}/${file}`;
+    if (fs.existsSync(join(distDir, typePath)) === false) {
+        console.warn(`Type definition file not found: ${typePath}`);
+    }
     if (exportPath.endsWith("index")) {
         exportPath = exportPath.replace(/\/index$/, "");
+
         return [
-            [`./${exportPath}.js`, `./${distDirName}/${file}`],
-            [`./${exportPath}`, `./${distDirName}/${file}`],
-            [`./${exportPath}/index.js`, `./${distDirName}/${file}`],
+            [`./${exportPath}.js`, { import: writeImportPath, types: writeTypePath }],
+            [`./${exportPath}`, { import: writeImportPath, types: writeTypePath }],
+            [`./${exportPath}/index.js`, { import: writeImportPath, types: writeTypePath }],
         ];
     }
     return [
-        [`./${exportPath}`, `./${distDirName}/${file}`],
-        [`./${exportPath}.js`, `./${distDirName}/${file}`],
+        [`./${exportPath}`, { import: writeImportPath, types: writeTypePath }],
+        [`./${exportPath}.js`, { import: writeImportPath, types: writeTypePath }],
     ];
 });
 const exportObject = {
-    ".": `./${distDirName}/index.js`,
+    ".": {
+        import: `./${distDirName}/index.js`,
+        types: `./${distDirName}/index.d.ts`,
+    },
     "./package.json": `./package.json`,
-    ...Object.fromEntries(map),
+    ...Object.fromEntries(
+        map.sort((a, b) => {
+            return String(a[0]).localeCompare(String(b[0]));
+        })
+    ),
 };
 const pkg = JSON.parse(fs.readFileSync("./package.json") + "");
 pkg.exports = exportObject;
 fs.writeFileSync("./package.json", JSON.stringify(pkg, null, 4) + "\n");
-
-console.dir(exportObject);
+console.log("Updated package.json exports field.");
+// console.dir(exportObject);
